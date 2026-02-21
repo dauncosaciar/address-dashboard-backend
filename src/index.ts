3430e96b10c038292085273a2e03e663dbb0de6c
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-02-21 16:41:08 -0300

    install colors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import colors from "colors";
 import server from "./server";
 
 const port = process.env.PORT || 3333;
 
-server.listen(port, () => console.log(`API REST running on port ${port}`));
+server.listen(port, () =>
+  console.log(colors.bold.bgMagenta(`API REST RUNNING ON PORT ${port}`))
+);
