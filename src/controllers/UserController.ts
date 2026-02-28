8e81df99676ae3370fd34329dd6b6caa7b298f4f
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-02-28 20:44:24 -0300

    add createUser method to UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,16 @@ import { Request, Response } from "express";
 import User from "../models/UserModel";
 
 export class UserController {
+  static createUser = async (req: Request, res: Response) => {
+    try {
+      const user = new User(req.body);
+      await user.save();
+      res.status(201).json({ message: "Usuario creado correctamente" });
+    } catch (error) {
+      res.status(500).json({ error: "Error al crear el usuario" });
+    }
+  };
+
   static getAllUsers = async (req: Request, res: Response) => {
     try {
       const users = await User.find({}).select("_id name lastName role email");
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import { UserController } from "../controllers/UserController";
 const router = Router();
 
 router.get("/", UserController.getAllUsers);
+router.post("/", UserController.createUser);
 
 export default router;
