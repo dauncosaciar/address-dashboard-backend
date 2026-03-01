961d779094b11ab08ef66a383a6b3db85e231b8f
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-03-01 16:40:25 -0300

    add body and param validation in userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,16 +1,47 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "../middlewares/validation";
 import { UserController } from "../controllers/UserController";
 
 const router = Router();
 
-router.post("/", UserController.createUser);
+router.post(
+  "/",
+  body("name").notEmpty().withMessage("El nombre del Usuario es obligatorio"),
+  body("lastName").notEmpty().withMessage("El apellido del Usuario es obligatorio"),
+  body("role").notEmpty().withMessage("El rol del Usuario es obligatorio"),
+  body("email").isEmail().withMessage("El email del Usuario no es válido"),
+  body("password").notEmpty().withMessage("La contraseña del Usuario es obligatoria"),
+  handleInputErrors,
+  UserController.createUser
+);
 
 router.get("/", UserController.getAllUsers);
 
-router.get("/:userId", UserController.getUserById);
+router.get(
+  "/:userId",
+  param("userId").isMongoId().withMessage("El ID del usuario no es válido"),
+  handleInputErrors,
+  UserController.getUserById
+);
 
-router.put("/:userId", UserController.updateUser);
+router.put(
+  "/:userId",
+  param("userId").isMongoId().withMessage("El ID del usuario no es válido"),
+  body("name").notEmpty().withMessage("El nombre del Usuario es obligatorio"),
+  body("lastName").notEmpty().withMessage("El apellido del Usuario es obligatorio"),
+  body("role").notEmpty().withMessage("El rol del Usuario es obligatorio"),
+  body("email").isEmail().withMessage("El email del Usuario no es válido"),
+  body("password").notEmpty().withMessage("La contraseña del Usuario es obligatoria"),
+  handleInputErrors,
+  UserController.updateUser
+);
 
-router.delete("/:userId", UserController.deleteUser);
+router.delete(
+  "/:userId",
+  param("userId").isMongoId().withMessage("El ID del usuario no es válido"),
+  handleInputErrors,
+  UserController.deleteUser
+);
 
 export default router;
