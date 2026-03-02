9af6c00d18819d78aa0cf7800ceca2b32c353439
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-03-02 17:56:32 -0300

    add code to createAddress method in AddressController

--- src/controllers/AddressController.ts
+++ src/controllers/AddressController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
+import Address from "../models/AddressModel";
 
 export class AddressController {
   static createAddress = async (req: Request, res: Response) => {
     try {
+      const address = new Address(req.body);
+      address.user = req.user._id;
+      req.user.addresses.push(address._id);
+      await Promise.allSettled([address.save(), req.user.save()]);
+      res.status(201).json({ message: "Dirección creada correctamente" });
     } catch (error) {
       res.status(500).json({ error: "Error al crear la dirección" });
     }
