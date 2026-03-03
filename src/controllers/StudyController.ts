687982b2734a7ee6a542670922fbc05f6b9cf0c1
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-03-03 15:17:34 -0300

    add code to createStudy method in StudyController

--- src/controllers/StudyController.ts
+++ src/controllers/StudyController.ts
@@ -4,7 +4,11 @@ import Study from "../models/StudyModel";
 export class StudyController {
   static createStudy = async (req: Request, res: Response) => {
     try {
-      res.json({ message: "Desde createStudy()" });
+      const study = new Study(req.body);
+      study.user = req.user._id;
+      req.user.studies.push(study._id);
+      await Promise.allSettled([study.save(), req.user.save()]);
+      res.status(201).json({ message: "Estudio creado correctamente" });
     } catch (error) {
       res.status(500).json({ error: "Error al crear el estudio" });
     }
