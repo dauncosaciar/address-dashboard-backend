e4435dbfe8a979b72de132f788fc56b94e73383a
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-03-06 15:39:36 -0300

    add login method code to generate an authToken and send it to client

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
 import User from "../models/UserModel";
+import { checkPassword } from "../utils/auth";
+import { generateJwt } from "../utils/jwt";
 
 export class AuthController {
   static login = async (req: Request, res: Response) => {
     try {
-      res.json({ message: "Desde login()" });
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      // Check if user exists
+      if (!user) {
+        const error = new Error("Usuario no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      // Check user password
+      const correctPassword = await checkPassword(password, user.password);
+
+      if (!correctPassword) {
+        const error = new Error("Contraseña incorrecta");
+        res.status(401).json({ error: error.message });
+        return;
+      }
+
+      // Generate JWT and send it to client
+      const authToken = generateJwt({ _id: user._id });
+      res.send(authToken);
     } catch (error) {
       res.status(500).json({ error: "Error al iniciar sesión" });
     }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middlewares/validation";
 import { AuthController } from "../controllers/AuthController";
 
 const router = Router();
 
-router.post("/login", AuthController.login);
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("El email de acceso no tiene un formato válido"),
+  body("password").notEmpty().withMessage("La contraseña de acceso es obligatoria"),
+  handleInputErrors,
+  AuthController.login
+);
 
 export default router;
