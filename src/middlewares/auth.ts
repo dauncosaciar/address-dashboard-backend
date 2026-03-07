7050805963c144fda04f44f3483df34050913437
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2026-03-07 14:27:02 -0300

    add authenticate middleware to protect routes

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User, { IUser } from "../models/UserModel";
+
+declare global {
+  namespace Express {
+    interface Request {
+      authenticatedUser?: IUser;
+    }
+  }
+}
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const bearer = req.headers.authorization;
+
+    if (!bearer) {
+      const error = new Error("No autorizado");
+      res.status(401).json({ error: error.message });
+      return;
+    }
+
+    const [, token] = bearer.split(" ");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (typeof decoded === "object" && decoded._id) {
+      const user = await User.findById(decoded._id).select(
+        "_id name lastName role email"
+      );
+
+      if (user) {
+        req.authenticatedUser = user;
+        next();
+      } else {
+        res.status(401).json({ error: "Token no válido" });
+        return;
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Token no válido" });
+  }
+};
